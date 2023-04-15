80c729202433b68c1135ab5749a9143ee2bdec9c
LuIsMoReNo7 <1483853@senati.pe>
2023-04-15 10:16:55 -0500

    usamos el getElementByID

--- HTML-13-04-23/script.js
+++ HTML-13-04-23/script.js
@@ -1,5 +1,14 @@
 function validar() {
-    var nombre, apellidos, correo, edad, country, number;
+    var nombre, apellidos, correo, edad, country, number, genero;
     nombre = document.getElementById("nombre").value;
-    
+    apellidos = document.getElementById("apellidos").value;
+    correo = document.getElementById("correo").value;
+    edad = document.getElementById("edad").value;
+    country = document.getElementById("country").value;
+    number = document.getElementById("number").value;
+    genero = document.getElementById("genero").value;
+
+    if(nombre === "") {
+        alert("El campo esta vacio");
+    }
 }
