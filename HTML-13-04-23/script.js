b3f44e281c9b0735fa4d234008232d05b88519c9
LuIsMoReNo7 <1483853@senati.pe>
2023-04-15 11:04:50 -0500

    mejoras en JS

--- HTML-13-04-23/script.js
+++ HTML-13-04-23/script.js
@@ -1,25 +1,28 @@
 function validar() {
-    var nombre, apellidos, correo, contraseña, edad, country, number, genero;
+    var nombre, apellidos, correo, password, edad, country, number, genero;
     nombre = document.getElementById("nombre").value;
     apellidos = document.getElementById("apellidos").value;
     correo = document.getElementById("correo").value;
-    contraseña = document.getElementById("password").value;
+    password = document.getElementById("password").value;
     edad = document.getElementById("edad").value;
     country = document.getElementById("country").value;
     number = document.getElementById("number").value;
     genero = document.getElementById("genero").value;
 
-    if(nombre === "", apellidos === "", correo === "", edad === "", country === "", number === "", genero === "" ) {
+    if(nombre === "", apellidos === "", correo === "", password === "", edad === "", country === "", number === "", genero === "" ) {
         alert("El campo esta vacio");
         return false;
     }
-    else if(nombre.lenght>10) {
+    else if(nombre.lenght>20) {
         alert("El nombre es muy largo")
     }
-    else if(apellidos.lenght>10) {
+    else if(apellidos.lenght>20) {
         alert("El apellido es muy largo")
     }
     else if(password.lenght>10) {
+        alert("El apellido es muy largo")
+    }
+    else if(number.lenght>9) {
         alert("El apellido es muy largo")
     }
 }
