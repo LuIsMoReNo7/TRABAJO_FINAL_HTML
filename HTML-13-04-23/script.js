41d7a90a68be663b6bcbdff3722c52d2f9554fac
LuIsMoReNo7 <1483853@senati.pe>
2023-04-16 18:17:25 -0500

    hacemos el fondo y el header para la pagina de login

--- HTML-13-04-23/script.js
+++ HTML-13-04-23/script.js
@@ -1,28 +1,28 @@
 function validar() {
-    var nombre, apellidos, correo, password, edad, country, number, genero;
-    nombre = document.getElementById("nombre").value;
+    var nombres, apellidos, correo, password, edad, country, telefono, genero;
+    nombres = document.getElementById("nombre").value;
     apellidos = document.getElementById("apellidos").value;
     correo = document.getElementById("correo").value;
     password = document.getElementById("password").value;
     edad = document.getElementById("edad").value;
     country = document.getElementById("country").value;
-    number = document.getElementById("number").value;
+    telefono = document.getElementById("number").value;
     genero = document.getElementById("genero").value;
 
-    if(nombre === "", apellidos === "", correo === "", password === "", edad === "", country === "", number === "", genero === "" ) {
-        alert("El campo esta vacio");
+    if(nombres === "", apellidos === "", correo === "", contraseña === "", edad === "", country === "", telefono === "", genero === "" ) {
+        alert("todos los campos son obligatorios");
         return false;
     }
-    else if(nombre.lenght>20) {
+    else if(nombres.lenght>20) {
         alert("El nombre es muy largo")
     }
     else if(apellidos.lenght>20) {
         alert("El apellido es muy largo")
     }
-    else if(password.lenght>10) {
-        alert("El apellido es muy largo")
+    else if(contraseña.lenght>10) {
+        alert("la contraseña es muy largo")
     }
-    else if(number.lenght>9) {
-        alert("El apellido es muy largo")
+    else if(isNam.lenght>9) {
+        alert("El telefono ingresado no es un numero")
     }
 }
